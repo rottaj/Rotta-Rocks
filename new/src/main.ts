2c292e14a7bec8b1dba6bf10ca01a848bf0d898f
rottaj <jackrotta12@gmail.com>
2022-06-02 15:11:04 -0400

    Added Cubs, Torus

--- new/src/main.ts
+++ new/src/main.ts
@@ -8,13 +8,27 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1,1,1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: false } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-camera.position.z = 5;
+camera.position.z = 25;
+
+const geometryTwo = new THREE.TorusGeometry( 10, 3, 16, 100 )
+const torus = new THREE.Mesh( geometryTwo, material )
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
+scene.add(pointLight);
 
 function animate() {
   requestAnimationFrame( animate )
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  cube.rotation.z += 0.01
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.01
+  torus.rotation.z += 0.01
   renderer.render( scene, camera )
 }
 animate()
