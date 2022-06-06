7fd9d1f1f65b24d457a242b66bffa0c2c651f8ce
rottaj <jackrotta12@gmail.com>
2022-06-06 15:29:33 -0400

    Overlay working, building containers

--- new/src/main.ts
+++ new/src/main.ts
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer( {alpha: true  } );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -13,8 +13,8 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 camera.position.z = 30;
 
-const geometryTorus = new THREE.TorusGeometry( 10, 0.1, 16, 100 )
-const torus = new THREE.Mesh( geometryTorus, material )
+//const geometryTorus = new THREE.TorusGeometry( 10, 0.1, 16, 100 )
+//const torus = new THREE.Mesh( geometryTorus, material )
 ///scene.add(torus)
 
 const pointLight = new THREE.PointLight(0xffffff);
