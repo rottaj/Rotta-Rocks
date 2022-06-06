93b3964b89797e1e3cc0c64a4357f7c73ef545b2
rottaj <jackrotta12@gmail.com>
2022-06-06 19:50:21 -0400

    Added timeline

--- new/src/main.ts
+++ new/src/main.ts
@@ -8,7 +8,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1,1,1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: false } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 camera.position.z = 30;
