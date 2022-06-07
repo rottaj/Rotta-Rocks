dbc674d116ac697b3e4acb6f76e44305f07ab7d6
rottaj <jackrotta12@gmail.com>
2022-06-06 22:21:35 -0400

    Updated timeline... need to fix vite config prob

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,19 @@
 /// <reference types="vite/client" />
+
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+declare var require: any
 declare module 'three';
+declare module 'definConfig';
+
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, "index.html"),
+        }
+    }
+  }
+})
