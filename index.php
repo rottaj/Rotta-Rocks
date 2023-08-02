da1df5c9a5ce663a10f8b6a59cc4c1ee369e50d6
Jack <jackrotta12@gmail.com>
2023-08-01 18:54:25 -0700

    Test

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>rottaj</title>
+    </head>
+    <body>
+        <h1>I'm Jack</h1>
+        <h3>A hobbyist programmer, and security researcher.</h3>
+        <p>Today is a wonderful day! <?php echo date('Y-m-d'); ?> </p>
+    </body>
+</html>
